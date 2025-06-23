4aa1cc39328974c51823a122bdb70cc2e12b7ecb
Dukdzuy <133631863+Dukdzuy@users.noreply.github.com>
2025-06-23 17:32:27 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
 import App from './App.tsx'
 
-// Initialize QueryClient
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1, // Retry failed requests once
-      staleTime: 5 * 60 * 1000, // Data stays fresh for 5 minutes
-    },
-  },
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </StrictMode>,
 )
 
